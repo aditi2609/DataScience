5de2705cb27efcbcf41d460913ea3b83e14fc056
aditi2609 <anpatil.stat@gmail.com>
2019-11-26 13:39:24 +0530

    0_Variable_data

---
+++ 0_Variables_datatypes.py
@@ -0,0 +1,19 @@
+a = 10
+print(a)
+print(type(a))
+
+b = 15.2
+print(b)
+print(type(b))
+
+c = "Codekul"
+print(c)
+print(type(c))
+
+d = True
+print(d)
+print(type(d))
+
+e = 'Ten'
+print(e)
+print(type(e))
