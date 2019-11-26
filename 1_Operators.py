e3a59621792274561cf9c3845d6fe4a298fd2c39
aditi2609 <anpatil.stat@gmail.com>
2019-11-26 13:29:54 +0530

    1_Operators

---
+++ 1_Operators.py
@@ -0,0 +1,58 @@
+#Assignment Operators:
+'''
+    =
+'''
+
+a = 10
+b = 20
+
+#Arithmetic Operators:
+'''
+    +
+    -
+    *
+    /
+    %
+    **
+'''
+
+c = a + b
+c = a - b
+c = a * b
+c = a / b
+c = a % b
+c = a ** b
+
+#Comparison Operators:
+'''
+    <
+    >
+    <=
+    >=
+    ==
+    !=
+'''
+
+a = 20
+c = a != b
+
+#Logical Operators:
+'''
+    and
+    or
+    not
+
+    p   q   and     or  not(p)
+    1   1   1       1   0
+    1   0   0       1   0
+    0   1   0       1   1
+    0   0   0       0   1
+'''
+c = not(a == 10) and (b == 20)
+
+a = True
+b = True
+
+c = a + b
+
+print(c)
